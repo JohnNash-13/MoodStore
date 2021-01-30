1e6a3d746202f8e4bda6a5f54332069a36314fc5
ricky <1030456532@qq.com>
2021-01-30 09:59:57 +0800

    公众号页面切图的完善

--- moodstore/src/components/Card.vue
+++ moodstore/src/components/Card.vue
@@ -1,18 +1,31 @@
 <template>
-	<div class="view">
-		<div class="card">
-			<div class="card-header">
-				<div class="time">2021/1/28 20:54</div>
-				<div class="cmd">图标</div>
+	<div class="view flex flex-row-center text-grave">
+		<div class="card bg-white flex flex-column-start rounded">
+			<div class="card-header flex flex-row-between align-items-center p-1">
+				<p class="time text-normal">2021/1/28 20:54</p>
+				<div class="operate">
+					<img src="../../public/icons/operate.png" class="icon"/>
+				</div>
 			</div>
-			<div class="card-body">
-				<div class="name">不打伞的苏轼</div>
-				<div class="pic"><img src="../../public/icons/index-selected.png"></div>
+			<div class="card-body flex flex-row-between align-items-center px-1">
+				<h1 class="title text-dark text-normal">不打伞的苏轼</h1>
+				<div class="pic">
+					<img src="../../public/icons/index-selected.png" class="cover"/>
+				</div>
 			</div>
-			<div class="card-footer">
-				<div>图标<span>28</span></div>
-				<div>图标<span>2</span></div>
-				<div>图标<span>0</span></div>
+			<div class="card-footer flex flex-row-start align-items-center p-1">
+				<div class="flex flex-row-start align-items-center">
+					<img src="../../public/icons/views.png" class="icon" />
+					<span>28</span>
+				</div>
+				<div class="flex flex-row-start align-items-center px-1">
+					<img src="../../public/icons/like.png" class="icon"/>
+					<span>2</span>
+				</div>
+				<div class="flex flex-row-start align-items-center">
+					<img src="../../public/icons/zaikan.png" class="icon"/>
+					<span>0</span>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -29,50 +42,22 @@
 	}
 </script>
 
+<style scoped="scoped" src="./../static/styles/milk.css"></style>
 <style lang="scss" scoped>
-	.view {
-		background: rgb(255, 254, 254);
-		display: flex;
-		justify-content: center;
-	}
-
-	.card {
-		width: 300px;
-		padding: 1rem 1.5rem;
-		background: rgb(255, 255, 255);
-		border-radius: 5px;
-		box-shadow: 0 0 3px #eee;
-	}
-
-	.card>div {
-		display: flex;
-		align-items: center;
-		padding: 0.2rem 0;
-	}
-
-	.card-header,
-	.card-body {
-		justify-content: space-between;
-		font-size: 0.8rem;
-	}
-
-	.card-body>.pic {
-		width: 2.5rem;
-		height: 2.5rem;
-	}
-
-	.card-body>.pic img {
-		height: 100%;
+	.view{
 		width: 100%;
-	}
-
-	.card-footer>div {
-		margin-right: 0.8rem;
-		color: #aaa;
-		font-size: 0.8rem;
-	}
-
-	.card-footer>div>span {
-		font-size: 0.5rem;
+		.card{
+			width: 94%;
+			height: 120px;
+			.card-header{
+				height: 20%;
+			}
+			.card-body{
+				height: 60%;
+			}
+			.card-footer{
+				height: 20%;
+			}
+		}
 	}
 </style>
--- moodstore/src/views/Official.vue
+++ moodstore/src/views/Official.vue
@@ -1,6 +1,29 @@
 <template>
 	<div class="container flex flex-column-start align-items-center bg-default">
-		<Card/>
+		<div class="type-group bg-white m-2 rounded flex flex-column-around">
+			<div class="type-row flex flex-row-around align-items-center">
+				<router-link to="/" class="type bg-pink rounded flex flex-row-start align-items-center">
+					<img src="../../public/icons/news.png" class="icon m-2"/>
+					<p class="text-large text-grave">图文</p>
+				</router-link>
+				<router-link to="/" class="type bg-blue rounded flex flex-row-start align-items-center">
+					<img src="../../public/icons/video.png" class="icon m-2"/>
+					<p class="text-large text-grave">视频</p>
+				</router-link>
+			</div>
+			<div class="type-row flex flex-row-around align-items-center">
+				<router-link to="/" class="type bg-purple rounded flex flex-row-start align-items-center">
+					<img src="../../public/icons/image.png" class="icon m-2"/>
+					<p class="text-large text-grave">图片</p>
+				</router-link>
+				<router-link to="/" class="type bg-yellow rounded flex flex-row-start align-items-center">
+					<img src="../../public/icons/voice.png" class="icon m-2"/>
+					<p class="text-large text-grave">语音</p>
+				</router-link>
+			</div>
+		</div>
+		<Card class="m-2"/>
+		<Card class="m-2"/>
 	</div>
 </template>
 
@@ -18,6 +41,17 @@
 <style scoped="scoped" lang="scss">
 	.container{
 		padding-bottom: 48px;
-		
+		.type-group{
+			width: 94%;
+			height: 160px;
+			.type-row{
+				width: 100%;
+				height: 42%;
+				.type{
+					width: 46%;
+					height: 100%;
+				}
+			}
+		}
 	}
 </style>
